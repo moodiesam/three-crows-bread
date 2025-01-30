b7701781dd04419224163e397a6957d02635297b
moodiesam <sampmcampbell@gmail.com>
2025-01-30 11:20:02 -0500

    very basic homepage with image and company name

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,20 @@
 import Image from "next/image";
+import { Playfair_Display } from "next/font/google";
+import crowPic from './public/crow_white.png'
+
+const playfairDisplay = Playfair_Display({
+  subsets: ["latin"],
+  weight: '400',
+})
 
 export default function Home() {
   return (
-    <div className="grid justify-center m-6">
-      <h1 className="text-4xl">Three Crows Bread</h1>
+    <div className={`grid justify-center items-center ${playfairDisplay.className} min-h-screen bg-black text-slate-50`}>
+      <h1 className="text-6xl italic">Three Crows Bread</h1>
+      <Image 
+        src={crowPic}
+        alt="Picture of a Crow"
+        />
     </div>
   );
 }
