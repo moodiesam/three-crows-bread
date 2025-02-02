13957269ddf4492efe2358a9299b99ec1c911cfa
moodiesam <sampmcampbell@gmail.com>
2025-02-01 19:47:31 -0500

    add crows to main page

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import { Playfair_Display } from "next/font/google";
-import crowPic from './public/crow_white.png'
+import crowWhite from './public/crow_white.png'
+import crowLightGrey from './public/crow_light_grey.png'
+import crowDarkGrey from './public/crow_dark_grey.png'
 
 const playfairDisplay = Playfair_Display({
   subsets: ["latin"],
@@ -9,12 +11,22 @@ const playfairDisplay = Playfair_Display({
 
 export default function Home() {
   return (
-    <div className={`grid justify-center items-center ${playfairDisplay.className} min-h-screen bg-black text-slate-50`}>
-      <h1 className="text-6xl italic">Three Crows Bread</h1>
-      <Image 
-        src={crowPic}
-        alt="Picture of a Crow"
+    <section className={`grid justify-center items-center ${playfairDisplay.className} min-h-screen bg-black text-slate-50`}>
+      <h1 className="text-6xl italic text-center">Three Crows Bread</h1>
+      <div className="flex">
+        <Image 
+          src={crowDarkGrey}
+          alt="Picture of a Crow"
         />
-    </div>
+        <Image 
+          src={crowLightGrey}
+          alt="Picture of a Crow"
+        />
+        <Image 
+          src={crowDarkGrey}
+          alt="Picture of a Crow"
+        />
+      </div>
+    </section>
   );
 }
